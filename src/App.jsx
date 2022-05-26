66455eeaccbfffaddda89b7e6e617f91f68274ec
Juan Raul Martinez Lopez <juggantiss@gmail.com>
2022-05-26 14:33:44 -0500

    Agregado enlace a cv

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,20 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hi, I'm Juan Raul Martinez Lopez</p>
+        <p>Hola, Soy Juan Raul Martinez Lopez</p>
         <p>Frontend Developer Jr</p>
+        <p>Este es mi portafolio, está en construcción...</p>
+        <p>
+          Mientras esperas puedes{" "}
+          <a
+            className="App-link"
+            href="https://drive.google.com/file/d/1aUAhm6cE_MA-oOYQlZBEs5Bhr9gc3CnB/view?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            descargar mi CV.
+          </a>
+        </p>
       </header>
     </div>
   );
